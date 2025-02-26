e1d5a201a81bb872bcd598dc0edad74b142f80a4
ChayanitBm <chayanit@amity.co>
2025-02-26 17:50:42 +0700

    Release/v3.11.8 (#102)
    * Release/v3.11.7 (#959)
    
    * fix: update displayName v3
    
    * chore(release): 3.11.7
    
    ---------
    
    Co-authored-by: bmo-amity-bot <developers@amity.co>
    
    * Release/v3.11.8 (#960)
    
    * fix: add refresh to liveObject
    
    * fix: add refresh to liveObject
    
    * chore(release): 3.11.8
    
    ---------
    
    Co-authored-by: bmo-amity-bot <developers@amity.co>
    
    * chore(release): 3.11.8
    
    ---------
    
    Co-authored-by: bmo-amity-bot <developers@amity.co>

--- src/v4/core/hooks/objects/usePost.ts
+++ src/v4/core/hooks/objects/usePost.ts
@@ -2,7 +2,7 @@ import { PostRepository } from '@amityco/ts-sdk';
 import useLiveObject from '~/v4/core/hooks/useLiveObject';
 
 const usePost = (postId?: string) => {
-  const { item, ...rest } = useLiveObject({
+  const { item, refresh, ...rest } = useLiveObject({
     fetcher: PostRepository.getPost,
     params: postId as string,
     shouldCall: !!postId,
@@ -10,6 +10,7 @@ const usePost = (postId?: string) => {
 
   return {
     post: item,
+    refresh,
     ...rest,
   };
 };
